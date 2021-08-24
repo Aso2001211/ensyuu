e083c6c01619b0ed25edd9bd666bec425c0bf5d7
yamas <Aso2001211>
2021-08-24 14:35:23 +0900

    switch文

--- js/marubatu.js
+++ js/marubatu.js
@@ -134,6 +134,21 @@ function batuInsert(basyo){
         case 4:
 
             break;
+        case 5:
+
+            break;
+        case 6:
+
+            break;
+        case 7:
+
+            break;
+        case 8:
+
+            break;
+        case 9:
+
+            break;
     }
 }
 
