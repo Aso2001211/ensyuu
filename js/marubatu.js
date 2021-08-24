3b2153e9a3d5d3ccd8c50a36e8ab679e5d682ea4
yamas <Aso2001211>
2021-08-24 14:30:57 +0900

    処理追加

--- js/marubatu.js
+++ js/marubatu.js
@@ -12,6 +12,8 @@ const marubatu7 = document.getElementById('img7');
 const marubatu8 = document.getElementById('img8');
 const marubatu9 = document.getElementById('img9');
 
+const turnMessage = document.getElementById('turn');
+
 marubatu1.addEventListener('click',areaCheck1);
 marubatu2.addEventListener('click',areaCheck2);
 marubatu3.addEventListener('click',areaCheck3);
@@ -26,7 +28,7 @@ marubatu9.addEventListener('click',areaCheck9);
 function areaCheck1(){
     num = plaice[0][0];
     if(num===0){
-        process(0,0);
+        process(0,0,1);
     }else{
         gameError();
     }
@@ -34,7 +36,7 @@ function areaCheck1(){
 function areaCheck2(){
     num = plaice[0][1];
     if(num===0){
-        process(0,1);
+        process(0,1,2);
     }else{
         gameError();
     }
@@ -42,7 +44,7 @@ function areaCheck2(){
 function areaCheck3(){
     num = plaice[0][2];
     if(num===0){
-        process(0,2);
+        process(0,2,3);
     }else{
         gameError();
     }
@@ -50,7 +52,7 @@ function areaCheck3(){
 function areaCheck4(){
     num = plaice[1][0];
     if(num===0){
-        process(1,0);
+        process(1,0,4);
     }else{
         gameError();
     }
@@ -58,7 +60,7 @@ function areaCheck4(){
 function areaCheck5(){
     num = plaice[1][1];
     if(num===0){
-        process(1,1);
+        process(1,1,5);
     }else{
         gameError();
     }
@@ -66,7 +68,7 @@ function areaCheck5(){
 function areaCheck6(){
     num = plaice[1][2];
     if(num===0){
-        process(1,2);
+        process(1,2,6);
     }else{
         gameError();
     }
@@ -74,7 +76,7 @@ function areaCheck6(){
 function areaCheck7(){
     num = plaice[2][0];
     if(num===0){
-        process(2,0);
+        process(2,0,7);
     }else{
         gameError();
     }
@@ -82,7 +84,7 @@ function areaCheck7(){
 function areaCheck8(){
     num = plaice[2][1];
     if(num===0){
-        process(2,1);
+        process(2,1,8);
     }else{
         gameError();
     }
@@ -90,7 +92,7 @@ function areaCheck8(){
 function areaCheck9(){
     num = plaice[2][2];
     if(num===0){
-        process(2,2);
+        process(2,2,9);
     }else{
         gameError();
     }
@@ -98,13 +100,50 @@ function areaCheck9(){
 
 
 //○×を入れる、フラグを逆にする
-function process (tate,yoko){
+function process (tate,yoko,basyo){
     if(fil===true){
+        maruInsert(basyo);
         plaice[tate][yoko]=1;
         fil=false;
     }else{
+        batuInsert(basyo);
         plaice[tate][yoko]=2;
         fil=true;
+    }
+    turnChange();
+}
+
+//○を挿入する
+function maruInsert(basyo){
+
+}
+
+
+//×を挿入する
+function batuInsert(basyo){
+    switch (basyo){
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+    }
+}
+
+
+//ターン変更処理
+function turnChange(){
+    if (fil===true){
+        turnMessage.innerText = '○のターンです';
+    }else{
+        turnMessage.innerText = '×のターンです';
     }
 }
 
