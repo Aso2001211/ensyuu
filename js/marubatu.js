9cb0906cfb878a12a37ad1c82ca26c3458f01746
yamas <Aso2001211>
2021-08-24 11:16:24 +0900

    idと処理

--- js/marubatu.js
+++ js/marubatu.js
@@ -1,1 +1,15 @@
+window.onload = function (){
+    let area =[[0,0,0],[0,0,0],[0,0,0]]
+}
+
+const marubatu1 = document.getElementById('img1');
+const marubatu2 = document.getElementById('img2');
+const marubatu3 = document.getElementById('img3');
+const marubatu4 = document.getElementById('img4');
+const marubatu5 = document.getElementById('img5');
+const marubatu6 = document.getElementById('img6');
+const marubatu7 = document.getElementById('img7');
+const marubatu8 = document.getElementById('img8');
+const marubatu9 = document.getElementById('img9');
+
 
